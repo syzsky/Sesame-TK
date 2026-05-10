ba308136c7f27e62db01d2ea9f32b29f005b8fe8
syzsky <ld1689@163.com>
2026-05-11 07:02:14 +0800

    sync time-wanderer code

---
+++ app/src/main/java/io/github/lazyimmortal/sesame/util/maps/MemberBenefitsMap.java
@@ -0,0 +1,8 @@
+package io.github.lazyimmortal.sesame.util.maps;
+
+public class MemberBenefitsMap extends IdMapManager {
+    @Override
+    public String thisFileName(){
+        return "MemberBenefitsMap.json";//会员权益兑换映射表
+    }
+}
