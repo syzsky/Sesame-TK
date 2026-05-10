60e936b33d152df4e708ac5b30078f1cada5fb9e
syzsky <ld1689@163.com>
2026-05-11 07:02:10 +0800

    sync time-wanderer code

---
+++ app/src/main/java/io/github/lazyimmortal/sesame/util/maps/BeachMap.java
@@ -0,0 +1,11 @@
+package io.github.lazyimmortal.sesame.util.maps;
+/**
+ * 沙滩ID映射工具类。
+ * 提供了一个线程安全的ID映射，支持添加、删除、加载和保存ID映射。
+ */
+public class BeachMap extends IdMapManager {
+    @Override
+    public String thisFileName() {
+        return "BeachMap.json";//海洋ID映射文件
+    }
+}
