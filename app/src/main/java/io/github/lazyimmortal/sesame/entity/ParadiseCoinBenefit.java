a9211965c2ebbecbe6ee581dc7b30613d980fb9d
syzsky <ld1689@163.com>
2026-05-11 07:02:59 +0800

    sync time-wanderer code

---
+++ app/src/main/java/io/github/lazyimmortal/sesame/entity/ParadiseCoinBenefit.java
@@ -0,0 +1,25 @@
+package io.github.lazyimmortal.sesame.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import io.github.lazyimmortal.sesame.util.maps.IdMapManager;
+import io.github.lazyimmortal.sesame.util.maps.ParadiseCoinBenefitIdMap;
+
+public class ParadiseCoinBenefit extends MapperEntity {
+
+    public ParadiseCoinBenefit(String i, String n) {
+        id = i;
+        name = n;
+    }
+
+    public static List<ParadiseCoinBenefit> getList() {
+        List<ParadiseCoinBenefit> list = new ArrayList<>();
+        Map<String, String> idSet = IdMapManager.getInstance(ParadiseCoinBenefitIdMap.class).getMap();
+        for (Map.Entry<String, String> entry: idSet.entrySet()) {
+            list.add(new ParadiseCoinBenefit(entry.getKey(), entry.getValue()));
+        }
+        return list;
+    }
+}
