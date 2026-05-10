1ccdee94b510c78118451e96b6e88fa6c3465343
syzsky <ld1689@163.com>
2026-05-11 07:02:00 +0800

    sync time-wanderer code

---
+++ app/src/main/java/io/github/lazyimmortal/sesame/util/maps/ReserveaMap.java
@@ -0,0 +1,7 @@
+package io.github.lazyimmortal.sesame.util.maps;
+public class ReserveaMap extends IdMapManager {
+    @Override
+    public String thisFileName() {
+        return "ReserveaMap.json";//保护地ID映射表
+    }
+}
