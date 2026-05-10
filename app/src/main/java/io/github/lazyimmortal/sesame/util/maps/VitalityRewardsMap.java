2c2c2d422043916539a02697f129b06dacbd09fc
syzsky <ld1689@163.com>
2026-05-11 07:02:08 +0800

    sync time-wanderer code

---
+++ app/src/main/java/io/github/lazyimmortal/sesame/util/maps/VitalityRewardsMap.java
@@ -0,0 +1,7 @@
+package io.github.lazyimmortal.sesame.util.maps;
+public class VitalityRewardsMap extends IdMapManager{
+    @Override
+    public String thisFileName(){
+        return "vitalityRewardsMap.json";//活力值兑换映射表
+    }
+}
