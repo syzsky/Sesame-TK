7acd38f255f9b39c91dc9a84368e1020003747fc
syzsky <ld1689@163.com>
2026-05-11 07:02:03 +0800

    sync time-wanderer code

---
+++ app/src/main/java/io/github/lazyimmortal/sesame/util/maps/ParadiseCoinBenefitIdMap.java
@@ -0,0 +1,8 @@
+package io.github.lazyimmortal.sesame.util.maps;
+
+public class ParadiseCoinBenefitIdMap extends IdMapManager {
+    @Override
+    public String thisFileName(){
+        return "paradiseCoinBenefitMap.json";//小鸡乐园道具兑换映射表
+    }
+}
